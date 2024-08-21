3d977f7a2dfebfdd01b908d58a4a70128e547f62
Wallace027dev <wallaceofc@hotmail.com>
2024-08-21 15:11:08 -0300

    feat: material da aula

---
+++ examples/app.js
@@ -0,0 +1,11 @@
+function ContaBancaria (titular, saldo){
+  this.titular = titular;
+  this.saldo = saldo;
+  
+
+  depositar = function (valor) {},
+
+  retirar = function (valor) {},
+
+  consultarSaldo = function () {}
+};
